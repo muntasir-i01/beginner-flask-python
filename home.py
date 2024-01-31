25e5c675d6a8cc6b1421a06e7d70fec8e38328b0
muntasir-i01 <muntasirm55@gmail.com>
2024-01-31 10:16:33 +0600

    Add user function

--- home.py
+++ home.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello this is my home page <h1>Home!</h1>"
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+    
+
 if __name__ == "__main__":
     app.run()
